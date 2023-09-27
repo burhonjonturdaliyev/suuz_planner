4e68b8d67a4d26d37effe76530c66fc0122d1f56
Burkhonjon Turdialiev <burhonjonturdaliyev2003@gmail.com>
2023-09-27 16:05:21 +0500

    Should change this subjects

--- lib/data/bba.dart
+++ lib/data/bba.dart
@@ -65,7 +65,7 @@ class BBA3 {
         teacher: ""),
     SubjectModel(
         time: "14:00\n14:55",
-        courseCode: 'Webinar',
+        courseCode: 'Webinar', //editing here
         fullName: '',
         teacher: ''),
     SubjectModel(
@@ -92,7 +92,7 @@ class BBA3 {
         teacher: StaffName().Tiwari),
     SubjectModel(
         time: "12:00\n12:55",
-        courseCode: 'Club Activity',
+        courseCode: 'Club Activity', //eding here
         fullName: '',
         teacher: ''),
     SubjectModel(
@@ -105,26 +105,21 @@ class BBA3 {
         courseCode: 'FAB102',
         fullName: BBA3subject().FAB102,
         teacher: StaffName().Ujjwal),
-    SubjectModel(
-        time: "15:00\n15:55",
-        courseCode: 'QTM203',
-        fullName: BBA3subject().QTM203,
-        teacher: StaffName().Sankar),
   ];
   List<SubjectModel> thursday=[
     SubjectModel(
         time: "10:00\n10:55",
-        courseCode: 'QTM203',
+        courseCode: 'QTM203', //editing here
         fullName: BBA3subject().QTM203,
         teacher: StaffName().Sankar),
     SubjectModel(
         time: "11:00\n11:55",
-        courseCode: 'GMG401',
+        courseCode: 'GMG401', //editing here
         fullName: BBA3subject().GMG401,
         teacher: StaffName().Sankar),
     SubjectModel(
         time: "12:00\n12:55",
-        courseCode: 'EIB002',
+        courseCode: 'EIB002', //editing here
         fullName: BBA3subject().EIB002,
         teacher: StaffName().Ujjwal),
     SubjectModel(
@@ -137,27 +132,22 @@ class BBA3 {
         courseCode: 'Club Activity',
         fullName: '',
         teacher: ''),
-    SubjectModel(
-        time: "15:00\n15:55",
-        courseCode: 'OPM301',
-        fullName: BBA3subject().OPM301,
-        teacher: StaffName().Tiwari),
   ];
   List<SubjectModel> friday=[
       SubjectModel(
         time: "09:00\n09:55",
         courseCode: 'FAB102',
-        fullName: BBA3subject().FAB102,
+        fullName: BBA3subject().FAB102, //editing here
         teacher: StaffName().Ujjwal),
     SubjectModel(
         time: "11:00\n11:55",
         courseCode: 'OPM301',
-        fullName: BBA3subject().OPM301,
+        fullName: BBA3subject().OPM301, //editing here
         teacher: StaffName().Tiwari),
     SubjectModel(
         time: "12:00\n12:55",
         courseCode: 'EIB002',
-        fullName: BBA3subject().EIB002,
+        fullName: BBA3subject().EIB002, //editing here
         teacher: StaffName().Ujjwal),
   ];
 }
